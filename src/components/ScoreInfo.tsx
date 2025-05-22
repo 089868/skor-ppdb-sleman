bf066226ad9c9de2788ad490fa16672b4954bb8d
vswi <122506901+vswi@users.noreply.github.com>
2025-05-22 21:00:31 +0700

    Update ScoreInfo.tsx

--- src/components/ScoreInfo.tsx
+++ src/components/ScoreInfo.tsx
@@ -5,21 +5,21 @@ interface ScoreInfoProps {
 const ScoreInfo = ({ totalScore }: ScoreInfoProps) => {
   return (
     <div className="text-sm text-gray-600 bg-gray-50 p-4 rounded-lg">
-      <p className="font-semibold mb-2">* SKOR TOTAL PPDB Sleman adalah gabungan:</p>
+      <p className="font-semibold mb-2">* SKOR TOTAL SPMB 2025 Sleman adalah gabungan:</p>
       <ul className="list-disc list-inside space-y-1">
-        <li>(Total Nilai ASPD × 60%)</li>
-        <li>(Total Rata-Rata Rapor × 40%)</li>
-        <li>Nilai Prestasi (Jika Ada)</li>
+        <li>(Total Nilai ASPD × 70%)</li>
+        <li>(Total Rata-Rata Rapor × 30%)</li>
+        <li>Nilai Prestasi (Jika Ada, di jalur prestasi)</li>
       </ul>
       <p className="mt-2">
-        Berdasarkan JUKNIS PPDB SMP NEGERI SLEMAN 2024-2025 di {" "}
+        Berdasarkan SOSIALISASI SPMB 2025 SMP NEGERI SLEMAN di {" "}
         <a 
-          href="https://smpn1prambanansleman.sch.id/wp-content/uploads/2024/05/Juknis-PPDB-SMP-2024-2025-Sleman.pdf"
+          href="https://www.youtube.com/watch?v=zyfb8kXlzZ8"
           className="text-blue-600 hover:underline"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Juknis PPDB SMP
+          Juknis SPMB 2025 SMP belum terbit
         </a>
       </p>
     </div>
